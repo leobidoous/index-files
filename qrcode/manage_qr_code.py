50cdeb45d4d23702f9c2de1f2a6606a390254eac
Rafael Alessandro <noprafa@gmail.com>
2021-01-19 09:26:18 -0300

    Verificação para a plataforma utilizada (Linux ou Windows)
    - Caso seja linux, é preciso instalar
       - sudo apt-get install libzbar0
       - sudo apt-get install poppler-utils
    - Caso seja Windows
       - Instalar poppler e identificar no método qual o caminho para o /bin/ do arquivo baixado

--- qrcode/manage_qr_code.py
+++ qrcode/manage_qr_code.py
@@ -5,6 +5,7 @@ from django.conf import settings
 import cv2, os, io, timeit, glob, shutil
 import logging
 import numpy as np
+from sys import platform
 from PIL import Image
 import pyzbar.pyzbar as pyzbar
 from base64 import b64encode, b64decode
@@ -129,9 +130,13 @@ class ManageQrCode:
         self.cropped_bytes_pdf = self.cropped_bytes_pdf.read()
 
     def pdf_to_image(self):
-        pdf_image = convert_from_bytes(self.cropped_bytes_pdf,
-                                       poppler_path=os.path.join(settings.BASE_DIR, 'venv', 'poppler-0.68.0', 'bin'),
-                                       dpi=1000)
+        if platform == 'linux' or platform == 'linux2':
+            pdf_image = convert_from_bytes(self.cropped_bytes_pdf, dpi=1000)
+        elif platform == 'win32':
+            pdf_image = convert_from_bytes(self.cropped_bytes_pdf,
+                                           poppler_path=os.path.join(settings.BASE_DIR, 'venv', 'poppler-0.68.0',
+                                                                     'bin'),
+                                           dpi=1000)
         # Rodando só o método
         # pdf_image = convert_from_bytes(self.cropped_bytes_pdf,
         #                               poppler_path=os.path.join('..', 'venv', 'poppler-0.68.0', 'bin'),
