c418a8b93bf228e9ec65f5d9dd0739bf0a9948cd
Rafael Alessandro <noprafa@gmail.com>
2021-01-22 11:00:23 -0300

    Mudando exibição do admin, serializer, adicionando app qrcode

--- qrcode/models.py
+++ qrcode/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
+import uuid
+from django.utils.html import format_html
+from base64 import b64decode
 # Create your models here.
+
+
+class DocumentModel(models.Model):
+    id = models.UUIDField(primary_key=True, verbose_name=_('uuid'), default=uuid.uuid4, unique=True)
+    name = models.CharField("Nome do documento", max_length=255)
+    qr_code = models.CharField("Código do QR Code", max_length=255)
+    qr_code_image = models.TextField("Imagem do QR Code (Base64)")
+
+    def image_thumb(self):
+        if self.qr_code_image:
+            return format_html(f'<img src="data:image/png;base64, {self.qr_code_image}" alt="QR Code" width="100"/>')
+    image_thumb.short_description = "Imagem do QR Code"
+
+    def __str__(self):
+        return str(self.name)
--- qrcode/views.py
+++ qrcode/views.py
@@ -1,3 +1,5 @@
+import glob
+import os
 
 from rest_framework import status
 from rest_framework.renderers import TemplateHTMLRenderer
@@ -7,6 +9,8 @@ from rest_framework.parsers import FileUploadParser
 
 # For managing qrcode
 from .manage_qr_code import ManageQrCode
+from .models import DocumentModel
+from .serializers import QrCodeSerializer
 
 
 class QrCodeView(APIView):
@@ -19,15 +23,64 @@ class QrCodeView(APIView):
         return Response()
 
     @staticmethod
+    def insert_pdf_in_db():
+        # Caminho para o pdf
+        path = ("C:\\Users\\Rafael\\Documents\\PDF_QRCODE\\enviados\\")
+        files_pdf = glob.glob(path + "*.pdf")
+
+        for pdf_path in files_pdf:
+            filter_name = os.path.split(pdf_path)[1]
+            if DocumentModel.objects.filter(name__icontains=filter_name):
+                print(f"Already exists on database the: {filter_name}")
+                continue
+            qr_code = ManageQrCode(pdf_path)
+            decoded_text = qr_code.get_decoded_text()
+            payload = {}
+            if decoded_text is not None:
+                payload['name'] = filter_name
+                payload['qr_code'] = decoded_text
+                try:
+                    payload['qr_code_image'] = qr_code.qr_code_image64.decode('utf-8')
+                except:
+                    pass
+                serializer = QrCodeSerializer(data=payload)
+                if serializer.is_valid():
+                    serializer.save()
+            else:
+                payload['decoded_text'] = "Seu QR Code não foi identificado"
+        return Response()
+
+    @staticmethod
     def post(request):
         if request.FILES['myfile']:
             myfile = request.FILES['myfile']
             qr_code = ManageQrCode(myfile.file)
             decoded_text = qr_code.get_decoded_text()
-            payload = {}    
+            payload = {}
+            documents = DocumentModel.objects.filter(name__icontains=myfile.name)
+
+            if documents:
+                for model in documents:
+                    payload['name'] = myfile.name
+                    payload['qr_code'] = model.qr_code
+                    payload['qr_code_image'] = model.qr_code_image
+                    break
+                return Response(payload, status=status.HTTP_200_OK)
+
             if decoded_text is not None:
-                payload['decoded_text'] = decoded_text
-                payload['image'] = qr_code.qr_code_image64.decode('utf-8')
+                payload['name'] = myfile.name
+                payload['qr_code'] = decoded_text
+                if qr_code.qr_code_image64 is not None:
+                    payload['qr_code_image'] = qr_code.qr_code_image64.decode('utf-8')
+                else:
+                    payload['qr_code_image'] = None
+                serializer = QrCodeSerializer(data=payload)
+
+                if DocumentModel.objects.filter(name__icontains=myfile.name):
+                    return Response(payload, status=status.HTTP_200_OK)
+
+                if serializer.is_valid():
+                    serializer.save()
             else:
                 payload['decoded_text'] = "Seu QR Code não foi identificado"
             return Response(payload, status=status.HTTP_200_OK)
