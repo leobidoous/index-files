01292f69f87de6b59a023f340d841f7554f288e8
Rafael Alessandro <noprafa@gmail.com>
2021-02-01 10:15:10 -0300

    Editando HTML's
    - base_login.html -> colocando direcionamento para o 'esqueci minha senha'
    base_view.html -> mudando ordem do navbar (collapse navbar fica por último agora)
    index.html -> mudando a exibição do botão que indica a ordenação das tabelas
    urls.py -> agora o logout redireciona para a tela de login e não para o home

--- core/urls.py
+++ core/urls.py
@@ -23,7 +23,7 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('api/', include('core.api.urls', namespace="api")),
-    path('logout/', LogoutView.as_view(next_page='core:home'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='core:login'), name='logout'),
     path('login/', LoginView.as_view(template_name='core/base_login.html'), name='login'),
     path('redefinir-senha', SolicitarRedefinirSenha.as_view(), name='reset_password'),
     path('redefinir-senha/done', RedefinirSenhaCompleto.as_view(), name='password_reset_done'),
