34dd323fb26b523672028c1fb697fceecc7c9b70
knowrafa <noprafa@gmail.com>
2021-03-29 13:18:58 -0300

    Adicionando try ao remover o arquivo

--- core/routine.py
+++ core/routine.py
@@ -19,7 +19,8 @@ from qrcode.manage_qr_code import ManageQrCode
 
 
 def processar_digitalizado_iop():
-    path = settings.PATH_FILES + settings.PATH_IOP
+    pasta = settings.PATH_IOP
+    path = settings.PATH_FILES + pasta
     files_pdf = glob.glob(path + "*.pdf")
     for pdf_path in files_pdf:
         filter_name = os.path.split(pdf_path)[1]
@@ -60,6 +61,8 @@ def processar_digitalizado_iop():
                 # Pega o último atualizado (por precaução)
                 arquivo = arquivo.order_by('-atualizado_em').first()
 
+            caminho_base = pasta + 'digitalizado/' + filter_name
+
             # Payload do IndexFile
             arquivo_indexado_dict = {
                 'nome': paciente.ds_pessoa_fisica,
@@ -76,7 +79,104 @@ def processar_digitalizado_iop():
                 'data_nascimento': paciente.dt_nascimento,
                 'genero': paciente.ie_sexo,
                 # http para debug e https para o servidor
-                'url': settings.SITE_NAME + settings.MEDIA_URL + settings.PATH_IOP + 'digitalizado/' + filter_name,
+                'url': settings.SITE_NAME + settings.MEDIA_URL + caminho_base,
+                'tipo_documento': 'd',
+            }
+
+            try:
+                if arquivo:
+                    arquivo_anterior = arquivo.url.split(settings.MEDIA_URL)[1]
+                    arquivo_indexado_serializer = ArquivoIndexadoRoutineSerializer(instance=arquivo,
+                                                                                   data=arquivo_indexado_dict,
+                                                                                   partial=True)
+                    try:
+                        os.remove(settings.PATH_MOVE_FILES_TO + arquivo_anterior)
+                    except Exception as e:
+                        pass
+                else:
+                    arquivo_indexado_serializer = ArquivoIndexadoRoutineSerializer(data=arquivo_indexado_dict)
+
+                arquivo_indexado_serializer.is_valid(raise_exception=True)
+                arquivo_indexado_serializer.save()
+
+                try:
+                    # shutil.copyfile(path + filter_name,
+                    #                 settings.PATH_MOVE_FILES_TO_LOCAL + pasta + 'digitalizado/' + filter_name)
+
+                    shutil.move(path + filter_name,
+                                settings.PATH_MOVE_FILES_TO + caminho_base)
+                except FileNotFoundError:
+                    pass
+
+
+            except Exception as e:
+                print(repr(e))
+
+        except TasyPatient.DoesNotExist:
+            pass
+
+def processar_digitalizado_domed():
+    pasta = settings.PATH_DOMED
+    path = settings.PATH_FILES + pasta
+    files_pdf = glob.glob(path + "*.pdf")
+    for pdf_path in files_pdf:
+        filter_name = os.path.split(pdf_path)[1]
+
+        qr_code = ManageQrCode(pdf_path)
+        decoded_text = qr_code.get_decoded_text()
+
+
+        if decoded_text is None:
+            # Ignora o resto da iteração caso o QR Code não tenha sido identificado ou não exista
+            continue
+        print(decoded_text)
+
+        codes = decoded_text.split('-')
+        nr_atendimento = codes[1]
+        payload = {'nr_atendimento': nr_atendimento}
+
+        try:
+            paciente = TasyPatient.objects.using('tasy_erp').get(nr_atendimento=int(nr_atendimento))
+            payload['paciente'] = paciente
+
+            ######### Inserindo dados do paciente aqui #################
+
+            # Cria um setor, estabelecimento e health insurance.
+            # Baseado no nome do setor de atendimento ou obtém um se já existir com esse nome
+            estabelecimento, created = Estabelecimento.objects.get_or_create(nome=paciente.ds_estabelecimento)
+            setor, created = Setor.objects.get_or_create(nome=paciente.ds_setor_atendimento,
+                                                         estabelecimento_id=estabelecimento.id)
+            convenio, created = Convenio.objects.get_or_create(nome=paciente.ds_convenio)
+
+            # Tenta encontrar um arquivo pré existente para efetuar um update nele (caso exista)
+            arquivo = ArquivoIndexado.objects.filter(nome=paciente.ds_pessoa_fisica,
+                                                     cpf=paciente.nr_cpf,
+                                                     numero_atendimento=paciente.nr_atendimento,
+                                                     numero_prontuario=paciente.nr_prontuario, )
+
+            if arquivo:
+                # Pega o último atualizado (por precaução)
+                arquivo = arquivo.order_by('-atualizado_em').first()
+
+            caminho_base = pasta + 'digitalizado/' + filter_name
+
+            # Payload do IndexFile
+            arquivo_indexado_dict = {
+                'nome': paciente.ds_pessoa_fisica,
+                'nome_arquivo': filter_name,
+                'cpf': paciente.nr_cpf,
+                'numero_prontuario': paciente.nr_prontuario,
+                'convenio': convenio.id,
+                'setor': setor.id,
+                'numero_atendimento': paciente.nr_atendimento,
+                'estabelecimento': estabelecimento.id,
+                'data_entrada': paciente.dt_entrada,
+                'data_arquivo': datetime.now(),
+                'uti': paciente.cd_unidade,
+                'data_nascimento': paciente.dt_nascimento,
+                'genero': paciente.ie_sexo,
+                # http para debug e https para o servidor
+                'url': settings.SITE_NAME + settings.MEDIA_URL + caminho_base,
                 'tipo_documento': 'd',
             }
 
@@ -97,10 +197,10 @@ def processar_digitalizado_iop():
 
                 try:
                     # shutil.copyfile(path + filter_name,
-                    #                 settings.PATH_MOVE_FILES_TO_LOCAL + settings.PATH_IOP + 'digitalizado/' + filter_name)
+                    #                 settings.PATH_MOVE_FILES_TO_LOCAL + pasta + 'digitalizado/' + filter_name)
 
                     shutil.move(path + filter_name,
-                                settings.PATH_MOVE_FILES_TO + settings.PATH_IOP + 'digitalizado/' + filter_name)
+                                settings.PATH_MOVE_FILES_TO + caminho_base)
                 except FileNotFoundError:
                     pass
 
@@ -110,7 +210,6 @@ def processar_digitalizado_iop():
 
         except TasyPatient.DoesNotExist:
             pass
-
 
 def processar_prontuario_iop():
     for index, path in enumerate(pathlib.Path(settings.PATH_FILES + settings.PATH_PRONTUARIOS).iterdir()):
