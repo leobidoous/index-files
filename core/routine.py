8aa7c0013be7b3ade62dbae58b022a717b332593
knowrafa <noprafa@gmail.com>
2021-03-30 10:36:36 -0300

    Atualizando requirements
    Editando rotina para rodar apenas com arquivos não encriptados

--- core/routine.py
+++ core/routine.py
@@ -4,9 +4,9 @@ import os
 import pathlib
 import shutil
 from datetime import datetime
-from io import StringIO, BytesIO
+from io import StringIO
 
-import pikepdf
+from PyPDF2 import PdfFileReader
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.conf import settings
 from pdfminer.converter import TextConverter
@@ -220,126 +220,19 @@ def processar_prontuario_iop():
         manager = PDFResourceManager()
         converter = TextConverter(manager, file_handle, laparams=LAParams(char_margin=0.01))
         interpreter = PDFPageInterpreter(manager, converter)
-
-        doc_bytes = BytesIO()
-        encrypted = False
-        with pikepdf.open(str(path)) as documento:
-            if documento.is_encrypted:
-                encrypted = True
-            documento.save(doc_bytes)
-        for page in PDFPage.get_pages(doc_bytes, maxpages=1, check_extractable=False):
+        fh = open(str(path), 'rb')
+        file = PdfFileReader(str(path))
+        if file.isEncrypted:
+            fh.close()
+            continue
+        for page in PDFPage.get_pages(fh, maxpages=1, check_extractable=False):
             interpreter.process_page(page)
+        fh.close()
 
         text = file_handle.getvalue()
         caminho_base = settings.PATH_IOP + 'prontuario/' + path.stem + '.pdf'
 
         text = text.split('Profissional')[0].split('\n')
-
-        primeiro_padrao = {
-            'nome': 55,
-            'data_nascimento': 56,
-            'genero': 57,
-            'cpf': 58,
-            'setor': 18,
-            'numero_prontuario': 27,
-            'data_entrada': 16,
-            'data_arquivo': 60,
-            'uti': 19,
-            'estabelecimento': 29,
-            'convenio': 17,
-            'numero_atendimento': 26,
-        }
-
-        segundo_padrao = {
-            'nome': 7,
-            'data_nascimento': 8,
-            'genero': 9,
-            'cpf': 10,
-            'setor': 32,
-            'numero_prontuario': 43,
-            'data_entrada': 30,
-            'data_arquivo': 29,
-            'uti': 33,
-            'estabelecimento': 45,
-            'convenio': 31,
-            'numero_atendimento': 42
-        }
-
-        for padrao in [primeiro_padrao, segundo_padrao]:
-            try:
-                arquivo_indexado_dict = {
-                    'nome_arquivo': path.name,
-                    'nome': text[padrao.get('nome')],
-                    'data_nascimento': datetime.strptime(text[padrao.get('data_nascimento')], '%d/%m/%Y'),
-                    'genero': text[padrao.get('genero')],
-                    'cpf': text[padrao.get('cpf')],
-                    'setor': text[padrao.get('setor')],
-                    'numero_prontuario': text[padrao.get('numero_prontuario')],
-                    'data_entrada': datetime.strptime(text[padrao.get('data_entrada')], '%d/%m/%Y %H:%M:%S') if text[padrao.get('data_entrada')] != '' else '',
-                    'data_arquivo': datetime.strptime(text[padrao.get('data_arquivo')], '%d/%m/%Y') if text[padrao.get('data_arquivo')] != '' else '',
-                    'uti': text[padrao.get('uti')],
-                    'url': settings.SITE_NAME + settings.MEDIA_URL + caminho_base,
-                    'tipo_documento': 'p',
-                }
-
-                arquivo = ArquivoIndexado.objects.filter(nome=text[padrao.get('nome')],
-                                                         cpf=text[padrao.get('cpf')],
-                                                         numero_prontuario=text[padrao.get('numero_prontuario')], )
-
-                if text[padrao.get('numero_atendimento')]:
-                    arquivo_indexado_dict['numero_atendimento'] = str(text[padrao.get('numero_atendimento')])
-                    if type(arquivo_indexado_dict['numero_atendimento']) == type(tuple()):
-                        arquivo_indexado_dict['numero_atendimento'] = arquivo_indexado_dict['numero_atendimento'][0]
-                    # Filtra por numero_atendimento
-                    if arquivo:
-                        arquivo.filter(numero_atendimento=arquivo_indexado_dict['numero_atendimento'])
-
-                if text[padrao.get('estabelecimento')]:
-                    estabelecimento, created = Estabelecimento.objects.get_or_create(
-                        nome=text[padrao.get('estabelecimento')])
-                    arquivo_indexado_dict['estabelecimento'] = estabelecimento.id
-                    if text[padrao.get('setor')]:
-                        setor, created = Setor.objects.get_or_create(nome=text[padrao.get('setor')],
-                                                                     estabelecimento_id=estabelecimento.id)
-                        arquivo_indexado_dict['setor'] = setor.id
-
-                if text[padrao.get('convenio')]:
-                    convenio, created = Convenio.objects.get_or_create(nome=text[padrao.get('convenio')])
-                    arquivo_indexado_dict['convenio'] = convenio.id
-
-                try:
-                    if arquivo:
-                        arquivo = arquivo.order_by('-atualizado_em').first()
-                        arquivo_anterior = arquivo.url.split(settings.SITE_NAME + settings.MEDIA_URL)[1]
-                        # Se existir uma query, então pega o último e atualiza
-                        indexed_file_serializer = ArquivoIndexadoRoutineSerializer(instance=arquivo,
-                                                                                   data=arquivo_indexado_dict,
-                                                                                   partial=True)
-                        if arquivo_anterior:
-                            os.remove(settings.PATH_MOVE_FILES_TO + arquivo_anterior)
-                    else:
-                        indexed_file_serializer = ArquivoIndexadoRoutineSerializer(data=arquivo_indexado_dict)
-                    indexed_file_serializer.is_valid(raise_exception=True)
-                    indexed_file_serializer.save()
-                    try:
-                        # shutil.copyfile(fh.name,
-                        #                 settings.PATH_MOVE_FILES_TO_LOCAL + caminho_base)
-
-                        shutil.move(str(path), settings.PATH_MOVE_FILES_TO + caminho_base)
-
-                    except Exception as e:
-                        print(repr(e))
-
-                    print(f"Arquivo {path.name} salvo com sucesso!!")
-
-                    break
-                except Exception as e:
-                    repr(e)
-            except Exception as e:
-                print(repr(e))
-
-
-        '''
         try:
 
             arquivo_indexado_dict = {
@@ -410,7 +303,6 @@ def processar_prontuario_iop():
         except Exception as e:
             repr(e)
             print("Impossível registrar o arquivo: " + path.name + ' || Estrutura inválida...')
-        '''
 
 
 def start():
