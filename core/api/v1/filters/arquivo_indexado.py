f56b78246d5eb78ee227fb75dca1b32eed550650
knowrafa <noprafa@gmail.com>
2021-03-31 15:06:36 -0300

    Alterando filtro de Date para DateTime
    Colocando range, pois assim as timezones são aplicadas.

--- core/api/v1/filters/arquivo_indexado.py
+++ core/api/v1/filters/arquivo_indexado.py
@@ -7,9 +7,13 @@ from core.models import ArquivoIndexado
 
 class ArquivoIndexadoFilter(filters.FilterSet):
     nome = filters.CharFilter(lookup_expr='icontains')
-    data_entrada = filters.DateFilter(lookup_expr='contains')
+    data_entrada = filters.DateTimeFilter(method='filter_data')
 
     class Meta:
         model = ArquivoIndexado
         fields = ('nome', 'numero_prontuario', 'numero_atendimento',
                   'cpf', 'estabelecimento', 'data_entrada', 'setor', 'tipo_documento')
+
+    @staticmethod
+    def filter_data(queryset, value):
+        return queryset.filter(data_entrada__range=[value, value + timedelta(days=1)])
